653dba33dcb0c775ef5a7beeafa2e094e6065094
mateuspim <mateuspimsantos@gmail.com>
2025-07-09 23:58:15 -0300

    Add button functionality

---
+++ Programming Theory Project/Assets/MenuUIHandler.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
